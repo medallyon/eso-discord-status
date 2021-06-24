c0a3faa4ca3f51e5088af51b8c71e9a0abb0c1e4
Tilman Raendchen <tilman@medallyon.me>
2021-06-24 18:47:14 +0100

    Version 0.4.2

--- AddOn/DiscordRichPresence.lua
+++ AddOn/DiscordRichPresence.lua
@@ -7,7 +7,7 @@ DRP.name = "DiscordRichPresence"
 DRP.meta = {
     name = "Discord Rich Presence",
     author = "@Medallyon#5012",
-    version = "0.4.1",
+    version = "0.4.2",
     website = "https://www.esoui.com/downloads/info2054-DiscordStatusUpdater.html"
 }
 
